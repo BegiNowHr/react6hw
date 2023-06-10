7bd1b092517f38f8e23ecfb2b9f089f1c64b16c0
User <you@example.com>
2023-06-10 13:40:57 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const Slider = () => {
+  const images = [
+    "./img/egypt.jpg",
+    "./img/italy.jpg",
+    "./img/tailand.jpg",
+  ]
+
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const goToPreviousSlide = () => {
+    setCurrentImageIndex(prevIndex =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
+  const goToNextSlide = () => {
+    setCurrentImageIndex(prevIndex =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="slider">
+      <button onClick={goToPreviousSlide}>Назад</button>
+      <img src={images[currentImageIndex]} alt={`Slide ${currentImageIndex + 1}`} />
+      <button onClick={goToNextSlide}>Вперед</button>
     </div>
   );
-}
+};
 
-export default App;
+export default Slider;
+
+
